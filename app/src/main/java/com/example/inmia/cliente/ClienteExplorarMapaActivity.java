3075fd3fa47ed8aad0f5a6fb09b23f62d48f1fe9
jasnai10 <a20223806@pucp.edu.pe>
2026-07-17 13:50:30 -0500

    fix: Recuperación de archivos de la presentación final

--- app/src/main/java/com/example/inmia/cliente/ClienteExplorarMapaActivity.java
+++ app/src/main/java/com/example/inmia/cliente/ClienteExplorarMapaActivity.java
@@ -1,5 +1,6 @@
 package com.example.inmia.cliente;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
